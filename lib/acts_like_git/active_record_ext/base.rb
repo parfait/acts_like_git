a673939f8d0ffd1998b0d4f2b32ba79367e9be00
Roman Le Negrate <roman.lenegrate@gmail.com>
2008-11-29 12:01:46 +0100

    Re-added a file that didn't make it into the previous commit (active_record_ext.rb).  Made model tracking optional: enable it by setting ALG.all_versioned_models to an array.

--- lib/acts_like_git/active_record_ext/base.rb
+++ lib/acts_like_git/active_record_ext/base.rb
@@ -6,7 +6,7 @@ module ActsLikeGit
           class_inheritable_accessor :git_settings
         end
       end
-  
+      
       # Define the details for connecting to your git repository with this
       # method.  Without these details there will be no connection to a
       # git repository, which is the whole point of the plugin.
@@ -20,13 +20,11 @@ module ActsLikeGit
       def versioning(*fields, &block)
         return unless ActsLikeGit.versioning_enabled?
   
+        ActsLikeGit.all_versioned_models |= [self.name] if ActsLikeGit.all_versioned_models
+        
         self.git_settings ||= ModelInit.new(self, &block)
         git_settings.versioned_fields = [fields].flatten
-  
-        unless ActsLikeGit.all_versioned_models.include?(self.name)
-          ActsLikeGit.all_versioned_models << self.name
-        end
-  
+        
         self.git_settings.versioned_fields.each do |column|
           git_read_method = "def #{column}; read_git_method('#{column}'); end"          
           evaluate_attribute_method column, git_read_method
@@ -34,7 +32,7 @@ module ActsLikeGit
           git_write_method = "def #{column}=(val); write_git_method('#{column}', val); end"          
           evaluate_attribute_method column, git_write_method
         end
-  
+        
         after_save    :git_commit
         after_destroy :git_delete
       end
